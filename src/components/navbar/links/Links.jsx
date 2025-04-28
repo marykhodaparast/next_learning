baefba338bbc504b2bdaa2c6120a85836869b670
marykhodaparast <maryam.kh1374@gmail.com>
2025-04-28 12:14:44 +0330

    Added some styles for mobileLinks and etc

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -45,7 +45,7 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "/login" }}></NavLink>
         )}
       </div>
-      <button onClick={()=> setOpen((prev) => !prev)}>Menu</button>
+      <button className={styles.menuButton} onClick={()=> setOpen((prev) => !prev)}>Menu</button>
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
